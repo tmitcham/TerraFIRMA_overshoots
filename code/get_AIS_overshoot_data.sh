033286849941ebe585cc8e35e783e367739159a5
Tom Mitcham <thomas.mitcham@northumbria.ac.uk>
2024-12-26 10:33:34 +0000

    Updating list of suites for getting data from MASS

--- code/get_AIS_overshoot_data.sh
+++ code/get_AIS_overshoot_data.sh
@@ -1,12 +1,11 @@
-# RUN_IDS=( cs568 cx209 cy837 cy838 cz374 cz375 cz376 cz377 cz378 cz944 da800 da697 da892 db223 dc251 dc051 dc052 dc248 dc249 db956 dc565 dd210 dc032 dc123 dc130 )
-
-# RUN_IDS=( cs568 cx209 cy837 cy838 cz375 cz376 cz377 dc051 dc052 df028 dc123 dc130 )
-
-# RUN_IDS=( df028 dc123 dc130 )
-
-# RUN_IDS=( cw988 cw989 )
-
-# RUN_IDS=( cw990 cy623 da914 da916 da917 )
+# cs495 cs568 cx209 cw988 cw989 cw990 cz826
+# cy837 cy838 cz374 cz375 cz376 cz377 cz378 
+# cz834 cz855 cz859 db587 db723 db731
+# da087 da266 db597 db733 dc324
+# cz944 di335 da800 da697 da892 db223 df453 de620 dc251 db956
+# dc051 dc052 dc248 dc249 dc565 dd210 dc032 df028 de621 dc123 dc130
+# df025 df027 df021 df023 dh541 dh859
+# de943 de962 de963 dk554 dk555 dk556
 
 RUN_IDS=( cx209 cs568 )
 
