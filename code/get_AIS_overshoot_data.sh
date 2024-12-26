0e35993bd7c56f4311070f435539a34d35445062
Tom Mitcham <thomas.mitcham@northumbria.ac.uk>
2024-12-26 15:34:58 +0000

    small fix

--- code/get_AIS_overshoot_data.sh
+++ code/get_AIS_overshoot_data.sh
@@ -7,7 +7,7 @@
 # df025 df027 df021 df023 dh541 dh859
 # de943 de962 de963 dk554 dk555 dk556
 
-RUN_IDS=( cs568 cx209 cw988 cw989 cw990 cy837 cy838 cz374 cz375 cz376 cz377 cz378 cz834 cz855 cz859 db587 db723 db731 da087 da266 db597 db733 dc324 cz944 di335 da800 da697 da892 db223 df453 de620 dc251 db956 dc051 dc052 dc248 dc249 dc565 dd210 dc032 df028 de621 dc123 dc130 df025 df027 df021 df023 dh541 dh859 de943 de962 de963 dk554 dk555 dk556 cs495 cz826)
+RUN_IDS=( cs568 cx209 cw988 cw989 cw990 cy837 cy838 cz374 cz375 cz376 cz377 cz378 cz834 cz855 cz859 db587 db723 db731 da087 da266 db597 db733 dc324 cz944 di335 da800 da697 da892 db223 df453 de620 dc251 db956 dc051 dc052 dc248 dc249 dc565 dd210 dc032 df028 de621 dc123 dc130 df025 df027 df021 df023 dh541 dh859 de943 de962 de963 dk554 dk555 dk556 cs495 cz826 )
 
 for ID in "${RUN_IDS[@]}"; do
   if [ ! -d ../raw_data/${ID} ]; then
