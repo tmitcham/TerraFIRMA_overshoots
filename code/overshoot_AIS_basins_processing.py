a410c3ba0c0112b3d1e03b1190682dbbd54b7e4b
Tom Mitcham <tom.mitcham@bristol.ac.uk>
2025-01-09 11:30:18 +0000

    Updating processing script ahead of figure production

--- code/overshoot_AIS_basins_processing.py
+++ code/overshoot_AIS_basins_processing.py
@@ -9,22 +9,65 @@ import numpy as np
 import pickle
 import sys
 
-# from amrfile import io as amrio
+####################################################################################
+
+id=("Ctrl","Ramp-Up 1""cs568", "cx209", "cw988", "cw989", "cw990", "cy837", "cy838", "cz374", "cz375", "cz376", "cz377", "cz378", 
+    "cz834", "cz855", "cz859", "db587", "db723", "db731", "da087", "da266", "db597", "db733", "dc324", 
+    "cz944", "di335", "da800", "da697", "da892", "db223", "df453", "de620", "dc251", "db956", 
+    "dc051", "dc052", "dc248", "dc249", "dc565", "dd210", "dc032", "df028", "de621", "dc123", "dc130", 
+    "df025", "df027", "df021", "df023", "dh541", "dh859", "de943", "de962", "de963", "dk554", "dk555", "dk556")
+
+count = 0
 
 ####################################################################################
 
+# Read atmosphere data
 
-id = ["cs568", "cx209", "cy837", "cy838", "cz375", "cz376", "cz377", "dc052", "dc051", "df028", 
-      "dc123", "dc130", "cw988", "cw989", "cw990","cy623", "da914", "da916", "da917"]
+atmos_d = {}
 
-run_type = ["Ctrl","Ramp-Up 1", "1.5C", "2C", "3C", "4C", "5C", "_ramp-down -4 1.5C 50yr", 
-            "_ramp-down -4 2C 50yr", "_ramp-down -4 3C 50yr", "_ramp-down -4 4C 50yr", 
-            "_ramp-down -4 5C 50yr", "Ramp-Up 2", "Ramp-Up 3", "Ramp-Up 4", "Hist 1", 
-            "Hist 2", "Hist 3", "Hist 4"]
+print("Getting atmosphere data...")
 
-runs = dict(zip(id, run_type)) 
+for i in id:
 
-count = 0
+    print(f"Working on {i}: {runs[i]}")
+
+    atmos_dir = f"/home/users/tm17544/gws_terrafirma/TerraFIRMA_overshoots/raw_data/{i}/atmos/"
+    atmos_files = sorted(os.listdir(atmos_dir))
+    atmos_files_matched = fnmatch.filter(atmos_files, f"{i}*.pp")
+
+    count = 0
+    
+    atmos_df = np.ndarray(shape = (len(atmos_files_matched),2))
+
+    for j in atmos_files_matched:
+        
+        filename = f"{atmos_dir}/{j}"
+        f = cf.read(filename)
+        temp = f[0]
+        temp_global_avg = temp.collapse('area: mean', weights=True)
+
+        if i == "cs568":
+            atmos_df[count, 0] = int(j[9:13])-100
+        else:
+            atmos_df[count, 0] = int(j[9:13])
+            
+        atmos_df[count, 1] = temp_global_avg.data
+
+        count = count + 1
+        
+    atmos_d[i] = atmos_df
+
+####################################################################################
+
+# Save atmosphere data
+
+if count > 0:
+
+    print("Saving atmosphere data to file...")
+
+    atmos_save_file = open('../processed_data/atmos_data.pkl', 'wb') 
+    pickle.dump(atmos_d, atmos_save_file) 
+    atmos_save_file.close() 
 
 ####################################################################################
 
@@ -38,7 +81,7 @@ for i in id:
 
     AIS_data = {}
 
-    print(f"Working on {i}: {runs[i]}")
+    print(f"Working on {i}...")
 
     AIS_stats = pd.read_csv(f"/gws/nopw/j04/terrafirma/tm17544/TerraFIRMA_overshoots/processed_data/new_{i}_AIS_basins_diagnostics.csv")
 
@@ -47,6 +90,26 @@ for i in id:
     
     else:
         AIS_stats.time = AIS_stats.apply(lambda x: int(x.filename[97:101]), axis=1)
+        
+    AIS_stats["global_T"] = np.nan
+
+    count = 0
+
+    for j in atmos_d["cx209"][:,0]:
+
+        year = int(j+1) # because BISICLES stores data for 01/01 of following year...
+        print(f"Working on year: {year}")
+        pos = np.where(AIS_stats.time==year)
+
+        if np.size(pos) > 0:
+            AIS_stats["global_T"].mask(AIS_stats["time"]==year, atmos_d["cx209"][count,1], inplace=True)
+        else:
+            print(f"Year {year} not found in AIS data for {i}")
+
+        count = count + 1
+
+    # Interpolate to fill NaN valures in global_delta_T
+    AIS_stats["global_T"].interpolate(method='linear', inplace=True)
 
     AIS_grounded_SMB = []
     AIS_floating_SMB = []
@@ -55,6 +118,8 @@ for i in id:
     AIS_grounded_vol = []
     AIS_floating_vol = []
     AIS_VAF = []
+    
+    count = 0
 
     for j in range(17):
         
