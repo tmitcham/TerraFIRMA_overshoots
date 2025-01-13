0006b2f806c60e254113654edc8026c5d3276171
Tom Mitcham <tom.mitcham@bristol.ac.uk>
2025-01-13 13:07:38 +0000

    code for processing testing data and making plots

--- code/testing_mask_resolution.py
+++ code/testing_mask_resolution.py
@@ -8,17 +8,18 @@ import matplotlib.pyplot as plt
 
 ####################################################################################
 
-id = ["cs568", "cx209", "cy837", "cy838", "cz375", "cz376", "cz377"]
+id = ["cx209", "cy837", "cy838", "cz375", "cz376", "cz377"]
 
-run_type = ["PI-Control","Ramp-Up", "1.5C Stab", "2C Stab", "3C Stab", "4C Stab", "5C Stab"]
+run_type = ["Ramp-Up", "1.5C", "2C", "3C", "4C", "5C"]
 
 runs = dict(zip(id, run_type)) 
 
-line_cols = ['#000000','#C30F0E','#0003C7','#168039','#FFE11A','#FA5B0F','#9C27B0']
-line_stys = ["solid","solid","solid","solid","solid","solid","solid"]
+line_cols = ['#C30F0E','#0003C7','#168039','#FFE11A','#FA5B0F','#9C27B0']
+line_stys = ["solid","solid","solid","solid","solid","solid"]
 
 basins = list(range(17))
-resolutions = ["500m", "1km", "2km", "4km", "8km"]
+#resolutions = ["500m", "1km", "2km", "4km", "8km"]
+resolutions = ["4km"]
 
 ####################################################################################
 
@@ -30,7 +31,7 @@ for res in resolutions:
 
     print("Loading ice sheet data from relevant file...")
 
-    with open(f"../processed_data/AIS_basins_data_{res}.pkl", 'rb') as file:
+    with open(f"../processed_data/testing_AIS_basins_data.pkl", 'rb') as file:
 
         icesheet_d = pickle.load(file)
     
@@ -49,17 +50,9 @@ for res in resolutions:
             plot_data = icesheet_d[i]
             
             initialVAF = icesheet_d["cx209"][basin].iloc[0,61]
-            initialVAFpi = icesheet_d["cs568"][basin].iloc[0,61]
-
-            if i == "cs568":
                 
-                #plt.plot(plot_data[basin].iloc[:,2] - 1850, (plot_data[basin].iloc[:,55] - initialVAFpi)*(918*1000/(1028*3.625e14)), label = runs[i], lw=0.8, color = line_cols[count], linestyle = line_stys[count])
-                plt.plot(plot_data[basin].iloc[:,2] - 1850, (plot_data[basin].iloc[:,61] - initialVAFpi)*(918*1000/(1028*3.625e14)))
-
-            else:
-                
-                #plt.plot(plot_data[basin].iloc[:,2] - 1850, (plot_data[basin].iloc[:,55] - initialVAF)*(918*1000/(1028*3.625e14)), label = runs[i], lw=0.8, color = line_cols[count], linestyle = line_stys[count])
-                plt.plot(plot_data[basin].iloc[:,2] - 1850, (plot_data[basin].iloc[:,61] - initialVAF)*(918*1000/(1028*3.625e14)))    
+            plt.plot(plot_data[basin].iloc[:,2] - 1850, (plot_data[basin].iloc[:,55] - initialVAF)*(918*1000/(1028*3.625e14)), label = runs[i], lw=0.8, color = line_cols[count], linestyle = line_stys[count])
+            #plt.plot(plot_data[basin].iloc[:,2] - 1850, (plot_data[basin].iloc[:,61] - initialVAF)*(918*1000/(1028*3.625e14)))    
 
             count = count + 1
 
