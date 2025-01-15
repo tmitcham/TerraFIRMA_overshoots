560386c5133212c3bd5de2bdfb0c6ef567452c05
Tom Mitcham <thomas.mitcham@northumbria.ac.uk>
2025-01-15 23:10:51 +0000

    Starting to tidy code

--- code/get_AIS_overshoot_data.sh
+++ code/get_AIS_overshoot_data.sh
@@ -1,3 +1,7 @@
+# Purpose: Download ice sheet plot files and surface termperature data from 
+# the TerraFIRMA overshoot simulations on MASS
+
+# List of suite IDs to download data for
 # (cs495) cs568 cx209 cw988 cw989 cw990 (cz826)
 # cy837 cy838 cz374 cz375 cz376 cz377 cz378 
 # cz834 cz855 cz859 db587 db723 db731
@@ -8,6 +12,8 @@
 # de943 de962 de963 dk554 dk555 dk556
 
 RUN_IDS=( cs568 cx209 cw988 cw989 cw990 cy837 cy838 cz374 cz375 cz376 cz377 cz378 cz834 cz855 cz859 db587 db723 db731 da087 da266 db597 db733 dc324 cz944 di335 da800 da697 da892 db223 df453 de620 dc251 db956 dc051 dc052 dc248 dc249 dc565 dd210 dc032 df028 de621 dc123 dc130 df025 df027 df021 df023 dh541 dh859 de943 de962 de963 dk554 dk555 dk556 cs495 cz826 )
+ICESHEETS="AIS" # AIS, GrIS, or Both
+ATMOS="Yes" # Yes or No
 
 for ID in "${RUN_IDS[@]}"; do
   if [ ! -d ../raw_data/${ID} ]; then
@@ -15,6 +21,18 @@ for ID in "${RUN_IDS[@]}"; do
     mkdir -p ../raw_data/${ID}/atmos
     mkdir -p ../raw_data/${ID}/icesheet
   fi
-  moo get -ig :crum/u-${ID}/chy.file/*AIS.hdf5 ../raw_data/${ID}/icesheet/
-  moo select -i surfaceT.query :crum/u-${ID}/apz.pp ../raw_data/${ID}/atmos/
+
+  if [ ICESHEETS == "AIS" ]; then
+    moo get -ig :crum/u-${ID}/chy.file/*AIS.hdf5 ../raw_data/${ID}/icesheet/
+  elif [ ICESHEETS == "GrIS" ]; then
+    moo get -ig :crum/u-${ID}/chy.file/*GrIS.hdf5 ../raw_data/${ID}/icesheet/
+  elif [ ICESHEETS == "Both" ]; then
+    moo get -ig :crum/u-${ID}/chy.file/*AIS.hdf5 ../raw_data/${ID}/icesheet/
+    moo get -ig :crum/u-${ID}/chy.file/*GrIS.hdf5 ../raw_data/${ID}/icesheet/
+  fi
+
+  if [ ATMOS == "Yes" ]; then
+    moo select -i surfaceT.query :crum/u-${ID}/apz.pp ../raw_data/${ID}/atmos/
+  fi
+  
 done
