abf4cfe17704a082cb8809c4d9389229e1ff766b
Tom Mitcham <tom.mitcham@bristol.ac.uk>
2025-01-20 10:57:35 +0000

    More edits to diagnostics code

--- code/icesheet_diagnostics.py
+++ code/icesheet_diagnostics.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Oct 03 10:48:31 2024
+Modified on Fri Jan 16 2025
 
 Run the BISICLES diagnostics tool for all plot.*.hdf5 files within a directory and export data to a csv file
 Run as: "python icesheet_diagnostics.py [directory] [csv_file] [mask]"
@@ -54,119 +55,46 @@ def construct_command(plot_file, csv_file, append=False, mask_file=None, mask_no
         cmd += f" mask_file={mask_file} mask_no_start={mask_no_start} mask_no_end={mask_no_end}"
     return cmd
 
-"""
-if len(sys.argv) == 1:
+####################################################################################
 
-    print('No directory specified. Select one by running the command "python new_diagnostics_AIS.py [directory]"')
-    sys.exit()
+args = parse_arguments()
 
-directory =  sys.argv[1]
+# Validate and process directory
+icesheet = args.icesheet
+directory = validate_directory(args.directory)
+csv_file = args.csv_file
+mask_file = None if args.mask == "0" else args.mask
+masked = mask_file is not None
+mask_no_start = args.mask_no_start
+mask_no_end = args.mask_no_end
 
-if not directory.endswith('/'):
-
-    directory = directory + '/'
-
-filetoolsPath = '/home/users/tm17544/Code/BISICLES/bisicles-uob/code/filetools/'
-filetoolDiags = 'diagnostics2d.Linux.64.c++.gfortran.DEBUG.OPT.GNU.ex'
-
-csv_file = sys.argv[2]
-
-masked = False
-maskFile = ''
-
-if len(sys.argv) == 4 and sys.argv[3] != '0':
-
-    masked = True
-    maskFile = sys.argv[3]
-
+print(f"Running icesheet diagnostics in directory: {directory}")
 if masked:
+    print(f"Using mask file: {mask_file}")
 
-    print("Running new_diagnostics_AIS.py for directory: " + directory + " with mask: " + maskFile)
-
-else:
-
-    print("Running new_diagnostics_AIS.py for directory: " + directory)
-
+# Process files
 count = 0
-
 for infile in sorted(os.listdir(directory)):
     
-    if infile.endswith("AIS.hdf5"):
+    if icesheet == "AIS" and infile.endswith("AIS.hdf5"):
+        plot_file = os.path.join(directory, infile)
 
-        if count == 0:
-
-            if masked:
-
-                diagsCommand = filetoolsPath + filetoolDiags + ' plot_file=' + directory + infile + ' out_file=/home/users/tm17544/gws_terrafirma/TerraFIRMA_overshoots/processed_data/' + csv_file + ' ice_density=918.0 water_density=1028.0 h_min=1.0e-3 mask_file=' + maskFile + ' mask_no_start=0 mask_no_end=16'
+    elif icesheet == "GrIS" and infile.endswith("GrIS.hdf5"):
+        plot_file = os.path.join(directory, infile)
         
-            else:
+    append = count > 0
+    diags_command = construct_command(plot_file, csv_file, append=append, mask_file=mask_file)
 
-                diagsCommand = filetoolsPath + filetoolDiags + ' plot_file=' + directory + infile + ' out_file=/home/users/tm17544/gws_terrafirma/TerraFIRMA_overshoots/processed_data/' + csv_file + ' ice_density=918.0 water_density=1028.0 h_min=1.0e-3'
-        
-        else:
-
-            if masked:
-                    
-                diagsCommand = filetoolsPath + filetoolDiags + ' plot_file=' + directory + infile + ' out_file=/home/users/tm17544/gws_terrafirma/TerraFIRMA_overshoots/processed_data/' + csv_file + ' -append ice_density=918.0 water_density=1028.0 h_min=1.0e-3 mask_file=' + maskFile + ' mask_no_start=0 mask_no_end=16'
-
-            else:
-                
-                diagsCommand = filetoolsPath + filetoolDiags + ' plot_file=' + directory + infile + ' out_file=/home/users/tm17544/gws_terrafirma/TerraFIRMA_overshoots/processed_data/' + csv_file + ' -append ice_density=918.0 water_density=1028.0 h_min=1.0e-3'
-        
-        count = count + 1
-        
-        print("Running diagnostics for file: " + infile)
-        
-        diagsOutput = subprocess.check_output(diagsCommand,shell=True)
-
-if count == 0:
-    print("No plot.*.hdf5 files found")
-else:
-    print(str(count) + " plot.*.hdf5 files found. Created diagnostics file " + csv_file)
-"""
-
-def main():
-    args = parse_arguments()
-
+    print(f"Running diagnostics for file: {infile}")
     try:
-        # Validate and process directory
-        icesheet = args.icesheet
-        directory = validate_directory(args.directory)
-        csv_file = args.csv_file
-        mask_file = None if args.mask == "0" else args.mask
-        masked = mask_file is not None
+        subprocess.check_output(diags_command, shell=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error while running diagnostics for {infile}: {e}")
+        continue  # Skip to the next file
+    count += 1
 
-        # Log the operation
-        print(f"Running icesheet diagnostics in directory: {directory}")
-        if masked:
-            print(f"Using mask file: {mask_file}")
-
-        # Process files
-        count = 0
-        for infile in sorted(os.listdir(directory)):
-            
-            if infile.endswith("AIS.hdf5"):
-                plot_file = os.path.join(directory, infile)
-                append = count > 0
-                diags_command = construct_command(plot_file, csv_file, append=append, mask_file=mask_file)
-
-                print(f"Running diagnostics for file: {infile}")
-                try:
-                    subprocess.check_output(diags_command, shell=True)
-                except subprocess.CalledProcessError as e:
-                    print(f"Error while running diagnostics for {infile}: {e}")
-                    continue  # Skip to the next file
-                count += 1
-
-        # Summary
-        if count == 0:
-            print("No plot.*.hdf5 files found in the directory.")
-        else:
-            print(f"{count} plot.*.hdf5 files processed. Diagnostics written to {csv_file}.")
-
-    except Exception as e:
-        print(f"Error: {e}")
-        sys.exit(1)
-
-if __name__ == "__main__":
-    main()
+# Summary
+if count == 0:
+    print("No plot.*.hdf5 files found in the directory.")
+else:
+    print(f"{count} plot.*.hdf5 files processed. Diagnostics written to {csv_file}.")
