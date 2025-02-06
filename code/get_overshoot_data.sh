e278f8d7976a94259ea9b01a01ddb99b12acd37d
Tom Mitcham <tom.mitcham@bristol.ac.uk>
2025-02-06 13:56:32 +0000

    just getting AIS data for now

--- code/get_overshoot_data.sh
+++ code/get_overshoot_data.sh
@@ -17,17 +17,17 @@
 # Alternative Pre-Industrial AIS UKESM suites: dc060 dc085
 
 ###############################################################################
-:'
-RUN_IDS=( 
-  cs568 cx209 cw988 cw989 cw990 cy837 cy838 cz374 cz375 cz376 
-  cz377 cz378 cz834 cz855 cz859 db587 db723 db731 da087 da266 
-  db597 db733 dc324 di335 da800 da697 da892 db223 df453 
-  de620 dc251 dc051 dc052 dc248 dc249 dc565 dd210
-  df028 de621 dc123 dc130 df025 df027 df021 df023 dh541 dh859 
-  dg093 dg094 dg095 de943 de962 de963 dm357 dm358 dm359
-)
-'
-ICESHEETS="Both" # Options: AIS, GrIS, or Both
+
+#RUN_IDS=( 
+#  cs568 cx209 cw988 cw989 cw990 cy837 cy838 cz374 cz375 cz376 
+#  cz377 cz378 cz834 cz855 cz859 db587 db723 db731 da087 da266 
+#  db597 db733 dc324 di335 da800 da697 da892 db223 df453 
+#  de620 dc251 dc051 dc052 dc248 dc249 dc565 dd210
+#  df028 de621 dc123 dc130 df025 df027 df021 df023 dh541 dh859 
+#  dg093 dg094 dg095 de943 de962 de963 dm357 dm358 dm359
+#)
+
+ICESHEETS="AIS" # Options: AIS, GrIS, or Both
 ATMOS="Yes" # Options: Yes or No
 
 ###############################################################################
