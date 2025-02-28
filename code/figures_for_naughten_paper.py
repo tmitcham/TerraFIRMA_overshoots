06ef16c0d0c01e6cc95a9e0724d0861b83fc864a
Tom Mitcham <tom.mitcham@bristol.ac.uk>
2025-02-28 08:22:51 +0000

    now process updated AIS data

--- code/figures_for_naughten_paper.py
+++ code/figures_for_naughten_paper.py
@@ -12,13 +12,13 @@ id=["cs568", "cx209", "cw988", "cw989", "cw990", "cy837", "cy838", "cz375", "cz3
     "cz834", "cz855", "db587", "db723", "db731", "da087", "da266", "db597", "db733", "dc324",
     "cz944", "di335", "da800", "da697", "da892", "db223", "df453", "de620", "dc251",
     "dc051", "dc052", "dc248", "dc249", "dc565", "dd210", "df028", "de621", "dc123", "dc130",
-    "df025", "df027", "df021", "df023", "dh541", "dh859", "de943", "de962", "de963"]
+    "df025", "df027", "df021", "df023", "dh541", "dh859", "de943", "de962", "de963","dg093", "dg094", "dg095"]
 
 run_type = ["ZE-0","Up8", "_Up8", "_Up8", "_Up8", "ZE-1.5", "ZE-2", "ZE-3", "ZE-4", "ZE-5", "ZE-6",
             "_ZE-1.5", "_ZE-2", "_ZE-3", "_ZE-4", "_ZE-5", "_ZE-1.5", "_ZE-2", "_ZE-3", "_ZE-4", "_ZE-5",
             "_X", "_X", "_X", "_X", "_X", "_X", "_X", "_X", "_X", "_X",
             "_X", "_X", "_X", "_X", "_X", "_X", "_X", "_X", "_X", "_X",
-            "_X", "_X", "_X", "_X", "_X", "_X", "_X", "_X", "_X", "_X", "_X"]
+            "_X", "_X", "_X", "_X", "_X", "_X", "_X", "_X", "_X", "_X", "_X","_X", "_X", "_X"]
             
 runs = dict(zip(id, run_type)) 
 
@@ -27,23 +27,23 @@ line_cols = ['#000000','#C30F0E','#C30F0E','#C30F0E','#C30F0E','#0003C7','#16803
              '#0003C7','#168039','#FFE11A','#FA5B0F','#9C27B0', '#0003C7','#168039','#FFE11A','#FA5B0F','#9C27B0',
              '#168039','#168039','#168039','#0003C7','#FA5B0F','#FFE11A','#FFE11A','#FFE11A','#9C27B0',
              '#168039','#0003C7','#0003C7','#FFE11A','#168039','#FA5B0F','#FFE11A','#FFE11A','#FA5B0F','#9C27B0',
-             '#168039','#168039','#FFE11A','#FFE11A','#FA5B0F','#FA5B0F','#964b00','#964b00','#964b00']
+             '#168039','#168039','#FFE11A','#FFE11A','#FA5B0F','#FA5B0F','#964b00','#964b00','#964b00','#9C27B0','#9C27B0','#9C27B0']
 
 '''
 line_cols = ['Blue','Black','Black','Black','Black','Lawngreen','Goldenrod','Red','Sienna','Pink','Indigo',
              'Lawngreen','Goldenrod','Red','Sienna','Pink', 'Lawngreen','Goldenrod','Red','Sienna','Pink',
              'Goldenrod','Goldenrod','Goldenrod','Lawngreen','Sienna','Red','Red','Red','Pink',
              'Goldenrod','Lawngreen','Lawngreen','Red','Goldenrod','Sienna','Red','Red','Sienna','Pink',
-             'Goldenrod','Goldenrod','Red','Red','Sienna','Sienna','Indigo','Indigo','Indigo']
+             'Goldenrod','Goldenrod','Red','Red','Sienna','Sienna','Indigo','Indigo','Indigo','Pink','Pink','Pink']
 '''
 line_stys = ["solid","solid","solid","solid","solid","solid","solid","solid","solid","solid","solid",
              "solid","solid","solid","solid","solid","solid","solid","solid","solid","solid",
              "dotted","dotted","dotted","dotted","dotted","dotted","dotted","dotted","dotted",
              "dashed","dashed","dashed","dashed","dashed","dashed","dashed","dashed","dashed","dashed",
-             "dashdot","dashdot","dashdot","dashdot","dashdot","dashdot","dotted","dashed","dashdot"]
+             "dashdot","dashdot","dashdot","dashdot","dashdot","dashdot","dotted","dashed","dashdot","dotted","dashed","dashdot"]
 
 
-basins = [8, 15] # 8 = Ross, 15 = Filchner-Ronne
+basins = [8, 10, 15] # 8 = Ross, 15 = Filchner-Ronne
 
 # Plot settings
 plt.rcParams.update({'font.size': 7.5})
@@ -52,7 +52,7 @@ plt.rcParams.update({'font.size': 7.5})
 
 # Load data from pickle file
 
-with open('./latest_AIS_basins_data.pkl', 'rb') as file:
+with open("C:/Users/tm17544/OneDrive - University of Bristol/Projects/TerraFIRMA/processed_data/AIS_data_overshoots_masked.pkl", 'rb') as file:
     icesheet_d = pickle.load(file)
 
 ####################################################################################
@@ -74,6 +74,9 @@ print("Starting VAF vs Time plots for each basin...")
 for basin in basins:
     
     plt.figure(figsize=(4, 3))
+    
+    icesheet_d["cw988"][basin] = icesheet_d["cw988"][basin].drop(index=icesheet_d["cw988"][basin].index[74], axis=0).reset_index(drop=True)
+
 
     count = 0
 
@@ -81,18 +84,18 @@ for basin in basins:
 
         plot_data = icesheet_d[i]
         
-        initialVAF = icesheet_d["cx209"][basin].iloc[0,55]
-        initialVAFpi = icesheet_d["cs568"][basin].iloc[0,55]
+        initialVAF = icesheet_d["cx209"][basin]["VAF"][0]
+        initialVAFpi = icesheet_d["cs568"][basin]["VAF"][0]
         
         if i == "cs568":
             
             # *(0.918/1e9) to convert from m^3 to Gt
-            plt.plot(plot_data[basin].iloc[:,2] - 1850, (plot_data[basin].iloc[:,55] - initialVAFpi)*(0.918/1e9), label = runs[i], lw=0.8, color = line_cols[count], linestyle = line_stys[count])
+            plt.plot(plot_data[basin]["time"] - 1850, (plot_data[basin]["VAF"] - initialVAFpi)*(0.918/1e9), label = runs[i], lw=0.8, color = line_cols[count], linestyle = line_stys[count])
 
         else:
             
             # *(0.918/1e9) to convert from m^3 to Gt
-            plt.plot(plot_data[basin].iloc[:,2] - 1850, (plot_data[basin].iloc[:,55] - initialVAF)*(0.918/1e9), label = runs[i], lw=0.8, color = line_cols[count], linestyle = line_stys[count])
+            plt.plot(plot_data[basin]["time"] - 1850, (plot_data[basin]["VAF"] - initialVAF)*(0.918/1e9), label = runs[i], lw=0.8, color = line_cols[count], linestyle = line_stys[count])
         
         count = count + 1
 
@@ -105,15 +108,21 @@ for basin in basins:
     
     if basin == 8:
 
-        plt.title("Ross Ice Shelf Basin")
+        plt.title("Ross Basin")
         
         plot_file_id = "Ross"
         
     elif basin == 15:
 
-        plt.title("Filchner-Ronne Ice Shelf Basin")
+        plt.title("Filchner-Ronne Basin")
         
         plot_file_id = "FRIS"
+
+    elif basin == 10:
+
+        plt.title("Amundsen Sea Basin")
+        
+        plot_file_id = "ASE"
         
 
     handles, labels = ax.get_legend_handles_labels()
@@ -129,8 +138,8 @@ for basin in basins:
     secax = ax.secondary_yaxis('right', functions=(mass2sle, sle2mass)) 
     secax.set_ylabel('Sea level contribution (mm)')
 
-    ax.set_xlim([0, 730])
+    ax.set_xlim([0, 750])
 
     print(f"Finished and saving VAF vs Time plot for basin {basin}...")
 
-    plt.savefig(f"{plot_file_id}_basin_VAFvsTime.png", dpi = 600,  bbox_inches='tight')
+    plt.savefig(f"C:/Users/tm17544/OneDrive - University of Bristol/Projects/TerraFIRMA/figures/{plot_file_id}_basin_VAFvsTime.png", dpi = 600,  bbox_inches='tight')
