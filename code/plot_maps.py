34714b16e82f34f9bb71dbaffa28026bc4da25f8
Tom Mitcham <tom.mitcham@bristol.ac.uk>
2025-03-13 14:20:07 +0000

    debugging

--- code/plot_maps.py
+++ code/plot_maps.py
@@ -54,12 +54,12 @@ if not animation:
 
     lo, hi = amrio.queryDomainCorners(ISFile, level)
 
-    x, y, H = amrio.readBox2D(ISFile, level, lo, hi, "thickness", order)[2]
-    dHdt = amrio.readBox2D(ISFile, level, lo, hi, "dThickness/dt", order)[2]
-    B = amrio.readBox2D(ISFile, level, lo, hi, "Z_base", order)[2]
-    Speed = amrio.readBox2D(ISFile, level, lo, hi, "Vel_magnitidue", order)[2]
-    xVel = amrio.readBox2D(ISFile, level, lo, hi, "xVel", order)[2]
-    yVel = amrio.readBox2D(ISFile, level, lo, hi, "yVel", order)[2]
+    x,y,H = amrio.readBox2D(ISFile, level, lo, hi, "thickness", order)#[2]
+    dHdt = amrio.readBox2D(ISFile, level, lo, hi, "dThickness/dt", order)#[2]
+    B = amrio.readBox2D(ISFile, level, lo, hi, "Z_base", order)#[2]
+    Speed = amrio.readBox2D(ISFile, level, lo, hi, "Vel_magnitidue", order)#[2]
+    xVel = amrio.readBox2D(ISFile, level, lo, hi, "xVel", order)#[2]
+    yVel = amrio.readBox2D(ISFile, level, lo, hi, "yVel", order)#[2]
 
     amrio.free(ISFile)
 
