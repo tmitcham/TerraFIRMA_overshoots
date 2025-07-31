f462c285663b26b75c16151695c18333b5b0915f
Tom Mitcham <tom.mitcham@bristol.ac.uk>
2025-07-31 14:10:57 +0100

    fixing error in filename

--- code/process_diagnostics_output.py
+++ code/process_diagnostics_output.py
@@ -60,7 +60,7 @@ elif suite_set == "historical_rampups":
 
 # Define ice sheet data filenames
 IS_filename_prefix = "/gws/nopw/j04/terrafirma/tm17544/TerraFIRMA_overshoots/processed_data/"
-IS_filename_suffix = f"_{icesheet}_diagnostics{'_masked' if basin_mask else ''}.csv"
+IS_filename_suffix = f"_{icesheet}_diagnostics_masked.csv"
 
 ####################################################################################
 
