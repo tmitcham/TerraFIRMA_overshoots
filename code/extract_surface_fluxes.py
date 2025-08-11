cf915052692c1e3cbf46c55e94fa36dbc086d6d2
Tom Mitcham <tom.mitcham@bristol.ac.uk>
2025-08-11 12:04:48 +0100

    New code to extract surface and basal fluxes for offline BISICLES runs

---
+++ code/extract_surface_fluxes.py
@@ -0,0 +1,33 @@
+####################################################################################
+
+import os
+import argparse
+import subprocess
+
+####################################################################################
+
+# Extract the surface and basal thickness source from UKESM output files and save them to a series of hdf5 files that can be read into BISICLES
+
+# Executable
+EXTRACT_EXEC="/gws/nopw/j04/terrafirma/tm17544/BISICLES_filetools/extract2d"
+
+# Options
+SUITE_ID="cs568"
+INPUT_DIR=f"/gws/nopw/j04/terrafirma/tm17544/TerraFIRMA_overshoots/raw_data/{SUITE_ID}/icesheet"
+OUTPUT_DIR=f"/gws/nopw/j04/terrafirma/tm17544/TerraFIRMA_overshoots/processed_data/pi_surface_flux_files"
+START_YEAR=1951
+END_YEAR=1980
+
+# File list
+FILE_LIST = [f"{INPUT_DIR}/bisicles_{SUITE_ID}c_{year}0101_plot-AIS.hdf5" for year in range(START_YEAR, END_YEAR + 1)]
+
+# Run the extract tool on selected files
+for input_file in FILE_LIST:
+
+    output_file = f"{OUTPUT_DIR}/SMB_BMB_{os.path.basename(input_file)}"
+
+    extract_command = f"{EXTRACT_EXEC} {input_file} {output_file} activeSurfaceThicknessSource activeBasalThicknessSource"
+
+    subprocess.check_output(extract_command, shell=True)
+
+print(f"Extracted surface and basal thickness sources from {len(FILE_LIST)} files into {OUTPUT_DIR}.")
